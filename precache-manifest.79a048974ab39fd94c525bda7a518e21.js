95e222f16d881c8fe1ca74cc5fe03bdad78cd930
kommunistMaou <kommunistmaou@gmail.com>
2019-07-07 21:39:41 +0300

    Updates

---
+++ precache-manifest.79a048974ab39fd94c525bda7a518e21.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd98bcacc2d1da72c32007377ea81446",
+    "url": "/upqode-test/index.html"
+  },
+  {
+    "revision": "a18fd13f66a900180300",
+    "url": "/upqode-test/static/css/main.2594b715.chunk.css"
+  },
+  {
+    "revision": "37be27763edafe7840a0",
+    "url": "/upqode-test/static/js/2.40bd3ae2.chunk.js"
+  },
+  {
+    "revision": "a18fd13f66a900180300",
+    "url": "/upqode-test/static/js/main.07956e43.chunk.js"
+  },
+  {
+    "revision": "c6eb629f7bdbb6e888f4",
+    "url": "/upqode-test/static/js/runtime~main.2e3262ee.js"
+  },
+  {
+    "revision": "7ac0284af22cb7fdfa33549a72f5144e",
+    "url": "/upqode-test/static/media/BEYNO.7ac0284a.woff"
+  },
+  {
+    "revision": "6a0eddaf57321a08c423d6415610302f",
+    "url": "/upqode-test/static/media/banner.6a0eddaf.jpg"
+  },
+  {
+    "revision": "8e4ffc4cb4b516dfa43630a06165b6ff",
+    "url": "/upqode-test/static/media/ipad-mockup.8e4ffc4c.png"
+  },
+  {
+    "revision": "8557b2a81a9b33196b304086288f7413",
+    "url": "/upqode-test/static/media/map.8557b2a8.jpg"
+  },
+  {
+    "revision": "045dc5916be20ef3c7b7c5b028916b3e",
+    "url": "/upqode-test/static/media/products-background.045dc591.jpg"
+  },
+  {
+    "revision": "42f459df43495dbfb6b09e31fa9a15c8",
+    "url": "/upqode-test/static/media/products-title.42f459df.png"
+  },
+  {
+    "revision": "e6690f1ae1d60318e04bb700f491ac67",
+    "url": "/upqode-test/static/media/title1.e6690f1a.png"
+  },
+  {
+    "revision": "03571d52664ee2ebd92b05298854cb17",
+    "url": "/upqode-test/static/media/title2.03571d52.png"
+  },
+  {
+    "revision": "2f3ad175e7c46ab8e741920a44040534",
+    "url": "/upqode-test/static/media/title3.2f3ad175.png"
+  },
+  {
+    "revision": "a2497e47de156864a1cd8cba6e70011a",
+    "url": "/upqode-test/static/media/title4.a2497e47.png"
+  },
+  {
+    "revision": "3dbc40d6988c123045aa7dbed8d9a6ea",
+    "url": "/upqode-test/static/media/title5.3dbc40d6.png"
+  }
+]);
